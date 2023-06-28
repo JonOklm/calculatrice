072f476810b2aad0d5cb5df964437fbb91e97efb
JonOklm <madou222002@outlook.com>
2023-06-28 14:46:19 +0200

    changement des var en let

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ const BUTTONS = document.querySelector("#button");
 
 BUTTONS.addEventListener = function(){
 
-var valeur1 = document.getElementById("valeur1").value;
+let valeur1 = document.getElementById("valeur1").value;
 
-var valeur2 = document.getElementById("valeur2").value;
+let valeur2 = document.getElementById("valeur2").value;
 
-var operateur = document.getElementById("operateur").value;
+let operateur = document.getElementById("operateur").value;
 
 if(operateur == "-"){
     alert(Number(valeur1) - Number(valeur2));
